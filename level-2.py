783f1f1ce1c8668392a8eb103d25b596c3ab5c86
Mitch Kroska <kromitj@gmail.com>
2017-02-11 00:44:51 -0700

    RECURSION

--- level-2.py
+++ level-2.py
@@ -1,16 +1,7 @@
-[ 0, 1, 2, 3, 4, 5, 6, 7]
-[ 8, 9,10,11,12,13,14,15]
-[16,17,18,19,20,21,22,23]
-[24,25,26,27,28,29,30,31]
-[32,33,34,35,36,37,38,39]
-[40,41,42,43,44,45,46,47]
-[48,49,50,51,52,53,54,55]
-[56,57,58,59,60,61,62,63]
 
+MOVES = [[-1,-2],[-2,-1],[-2,1],[-1,2],[1,2],[2,1],[2,-1],[1,-2]]
 
-# Moves: [x, y]
-     
-[[ 0, 1, 2, 3, 4, 5, 6, 7],
+BOARD = [[0, 1, 2, 3, 4, 5, 6, 7],
 [ 8, 9,10,11,12,13,14,15],
 [16,17,18,19,20,21,22,23],
 [24,25,26,27,28,29,30,31],
@@ -19,9 +10,13 @@
 [48,49,50,51,52,53,54,55],
 [56,57,58,59,60,61,62,63]]
 
+# Moves: [x, y]
+     
+# BOARD = [[0 , 1, 2,3],
+#          [4 , 5, 6,7],
+#          [8 , 9,10,11],
+#          [12,13,14,15]]
 
-def answer(src, dest):
-    # your code here
 
 
 def find_value(board, value):
@@ -30,38 +25,105 @@ def find_value(board, value):
             if el == value:
                 return [board.index(row), row.index(el)] 
 
-MOVES = [[-2,-1],[-1, -2],[1, -2],[2, -1],[2, 1],[1, 2],[-1, 2],[-2, 1]]
+# dest = 2
+def move_valid(poss_move, places_been): 
+    # [1,-1]
+    return x_valid(poss_move[1]) and y_valid(poss_move[0]) and havnt_been_before(poss_move, places_been)
 
-def move_valid(player_loc, move): 
-    #move = [-2,-1]
-    #playerloc = [2, 4]
-    return x_valid(move[0], player_loc[1]) and y_valid(move[1], player_loc[0])
+def havnt_been_before(poss_move, places_been):
+    for place in places_been:
+        if place == poss_move:
+            return False
+    return True
     
-def x_valid(x_move, player_loc_x):
-# -2, 4
-return (player_loc_x + x_move) >= 0
+def x_valid(x_move):
+    board_width = len(BOARD[0])
+    return x_move >= 0 and x_move < board_width
 
 
 
-def y_valid(y_move, player_loc_y):
-    # -1, 2
-    return (player_loc_y + y_move) >= 0 
+def y_valid(y_move):
+    board_height = len(BOARD)
+    return y_move >= 0 and y_move < board_height
+
+# def player_move(player_loc, moves, move_num, dest, places_been):
+#     if move_num == len(moves):
+#         return None
+#     poss_move = calc_move(player_loc, moves[move_num])
+#     if move_valid(poss_move, places_been): 
+#         if player_on_destination(dest, poss_move):
+#             places_been.append(player_loc)
+#             return len(places_been)
+#         else:
+#             places_been.append(player_loc)
+#             dest_found = player_move(poss_move, moves, 0, dest, places_been)
+#             if dest_found:
+#                 return dest_found
+#             else:
+#                 move_num+=1
+#                 return player_move(player_loc, moves, move_num, dest, places_been)
+#     else:
+#         move_num+=1
+#         return player_move(player_loc, moves, move_num, dest, places_been)
 
 
 
-def player_move(player_loc, move, dest)
-    if move_valid(player_loc, move):
-        player_loc = move_player(player_loc, move)
-        if player_on_destination(dest, player_loc):
+def player_move(player_loc, moves, move_num, dest, places_been):
+    if move_num == len(moves):
+        return None
+    poss_move = calc_move(player_loc, moves[move_num])
+    if move_valid(poss_move, places_been): 
+        if player_on_destination(dest, poss_move):
+            places_been.append(player_loc)
+            return places_been
+        else:
+            places_been.append(player_loc)
+            response = player_move(poss_move, moves, 0, dest, places_been)
+            if response != None:
+                return response 
+    move_num+=1
+    return player_move(player_loc, moves, move_num, dest, places_been)
 
-end
 
-def move_player(player_loc, move):
-    return [(b[0]+a[1]), (b[1]+a[0])]
+def player_moves(player_loc, moves, move_num, dest, places_been):  
+    last_move = moves[len(moves)-1]
+    shortest = 10000000000000
+    move_num = move_num
+    for move in moves:
+        result = player_move(player_loc, moves, move_num, dest, places_been)
+        print(result)
+        if (result < shortest) and result != None:
+            shortest = result
+        if move == last_move:
+            return shortest   
+        move_num+=1
+
+
+
+
+def calc_move(player_loc, move):
+    return [(player_loc[0] + move[0]), (player_loc[1] + move[1])]
 
 
 def player_on_destination(dest, player_loc):
     return dest == BOARD[player_loc[0]][player_loc[1]]
+
+
+result = player_move([2,3], MOVES, 0, 39, [])
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
